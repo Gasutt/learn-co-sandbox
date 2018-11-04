e338ea40cfb400843e04fecd3cf83eb7196dcfb7
Georgia Sutter <gasutter5@gmail.com>
2018-11-04 20:49:50 +0000

    automatically backed up by learn

---
+++ badges.rb
@@ -0,0 +1,20 @@
+def badge_maker(name)
+ puts "Hello " + name + "."
+end
+def batch_badge_creator(name)
+puts "Hello #{name[0]}."
+puts "Hello #{name[1]}."
+puts "Hello #{name[2]}."
+puts "Hello #{name[3]}."
+puts "Hello #{name[4]}."
+puts "Hello #{name[5]}."
+puts "Hello #{name[6]}."
+end
+def assign_rooms(name,index)  
+name each_with_index 
+puts "Hello, name[index]! You will be assigned to room"
+end
+end
+end
+
+  
---
+++ display_board.rb
@@ -0,0 +1,6 @@
+x=1
+
+100000.times do
+  puts "hi"
+end
+puts x
